b6951401342e48495a3273e6e527a888c4a87853
Rhys Mainwaring <rhys.mainwaring@me.com>
2022-02-14 12:16:44 +0000

    [Ignition] experiments with custom shaders
    - Add code from ShaderParams system plugin to investigate adding custom shaders
    - There are issues with loading more that one visual plugin, this is an interim work-around
    
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- ign-marine/src/systems/waves/WavesVisual.cc
+++ ign-marine/src/systems/waves/WavesVisual.cc
@@ -235,8 +235,42 @@ using namespace systems;
 
 class ignition::gazebo::systems::WavesVisualPrivate
 {
-  /// \brief Destructor
-  public: ~WavesVisualPrivate();
+  ////////// BEGIN SHADER_PARAM
+
+  /// \brief Data structure for storing shader param info
+  public: class ShaderParamValue
+  {
+    /// \brief shader type: vertex or fragment
+    public: std::string shader;
+
+    /// \brief variable type: int, float, float_array, int_array,
+    ///        texture, texture_cube
+    /// \todo(anyone) support samplers
+    public: std::string type;
+
+    /// \brief variable name of param
+    public: std::string name;
+
+    /// \brief param value
+    public: std::string value;
+  };
+
+  /// \brief Path to vertex shader
+  public: std::string vertexShaderUri;
+
+  /// \brief Path to fragment shader
+  public: std::string fragmentShaderUri;
+
+  /// \brief A list of shader params
+  public: std::vector<ShaderParamValue> shaderParams;
+
+  /// \brief Time params that will be updated every iteration
+  public: std::vector<ShaderParamValue> timeParams;
+
+  ////////// END SHADER_PARAM
+
+  /// \brief Path to the model
+  public: std::string modelPath;
 
   /// \brief All rendering operations must happen within this call
   public: void OnUpdate();
@@ -262,6 +296,10 @@ class ignition::gazebo::systems::WavesVisualPrivate
 
   /// \brief Entity id of the visual
   public: Entity entity = kNullEntity;
+
+
+  /// \brief Entity id of the ocean visual
+  public: Entity oceanEntity = kNullEntity;
 
   /// \brief Current sim time
   public: std::chrono::steady_clock::duration currentSimTime;
@@ -308,10 +346,73 @@ void WavesVisual::Configure(const Entity &_entity,
   // function and _sdf is a const shared pointer to a const sdf::Element.
   auto sdf = const_cast<sdf::Element *>(_sdf.get());
 
+  ////////// BEGIN SHADER_PARAM
+
+  if (sdf->HasElement("param"))
+  {
+    // loop and parse all shader params
+    sdf::ElementPtr paramElem = sdf->GetElement("param");
+    while (paramElem)
+    {
+      if (!paramElem->HasElement("shader") ||
+          !paramElem->HasElement("name"))
+      {
+        ignerr << "<param> must have <shader> and <name> sdf elements"
+               << std::endl;
+        paramElem = paramElem->GetNextElement("param");
+        continue;
+      }
+      std::string shaderType = paramElem->Get<std::string>("shader");
+      std::string paramName = paramElem->Get<std::string>("name");
+
+      std::string type = paramElem->Get<std::string>("type", "float").first;
+      std::string value = paramElem->Get<std::string>("value", "").first;
+
+      WavesPrivate::ShaderParamValue spv;
+      spv.shader = shaderType;
+      spv.name = paramName;
+      spv.value = value;
+      spv.type = type;
+      this->dataPtr->shaderParams.push_back(spv);
+      paramElem = paramElem->GetNextElement("param");
+    }
+  }
+
+  // parse path to shaders
+  if (sdf->HasElement("shader"))
+  {
+    sdf::ElementPtr shaderElem = sdf->GetElement("shader");
+    if (!shaderElem->HasElement("vertex") ||
+        !shaderElem->HasElement("fragment"))
+    {
+      ignerr << "<shader> must have <vertex> and <fragment> sdf elements"
+             << std::endl;
+    }
+    else
+    {
+      auto modelEntity = topLevelModel(_entity, _ecm);
+      this->dataPtr->modelPath =
+          _ecm.ComponentData<components::SourceFilePath>(modelEntity).value();
+      sdf::ElementPtr vertexElem = shaderElem->GetElement("vertex");
+      this->dataPtr->vertexShaderUri = common::findFile(
+          asFullPath(vertexElem->Get<std::string>(),
+              this->dataPtr->modelPath));
+      sdf::ElementPtr fragmentElem = shaderElem->GetElement("fragment");
+      this->dataPtr->fragmentShaderUri = common::findFile(
+          asFullPath(fragmentElem->Get<std::string>(),
+              this->dataPtr->modelPath));
+    }
+  }
+
+  ////////// END SHADER_PARAM
+
   // capture entity 
   this->dataPtr->entity = _entity;
   auto nameComp = _ecm.Component<components::Name>(_entity);
   this->dataPtr->visualName = nameComp->Data();
+
+  // create new entity for the ocean visual
+  this->dataPtr->oceanEntity = _ecm.CreateEntity();
 
   // connect to the SceneUpdate event
   // the callback is executed in the rendering thread so do all
@@ -368,6 +469,9 @@ enum class OceanVisualMethod : uint16_t
   /// \brief Ogre2DynamicGeometry
   OGRE2_DYNAMIC_GEOMETRY = 3,
 
+  /// \brief Ogre2DynamicGeometry
+  PLANE = 4,
+
   /// \internal
   /// \brief Indicator used to create an iterator over the
   /// enum. Do not use this.
@@ -412,6 +516,7 @@ void WavesVisualPrivate::OnUpdate()
         if (value && *value == static_cast<int>(this->entity))
         {
           this->visual = std::dynamic_pointer_cast<rendering::Visual>(n);
+          ignmsg << "Waves: found visual [" << this->visual->Name() << "]\n";
           break;
         }
       }
@@ -438,18 +543,126 @@ void WavesVisualPrivate::OnUpdate()
   double simTime = (std::chrono::duration_cast<std::chrono::nanoseconds>(
       this->currentSimTime).count()) * 1e-9;
 
-  // ocean tile parameters
-  int N = 128;        // tile resolution
-  double L = 256.0;   // tile size
-  double u = 5.0;    // wind strength
-
-  OceanVisualMethod method = OceanVisualMethod::OGRE2_DYNAMIC_GEOMETRY;
+  OceanVisualMethod method = OceanVisualMethod::OGRE2_MESH;
   switch (method)
   {
     case OceanVisualMethod::OGRE2_DYNAMIC_GEOMETRY:
     {
-      // Test attaching another visual to the entity
-      if (this->oceanVisuals.empty())
+      ignmsg << "Waves: creating dynamic geometry ocean visual\n";
+
+      // create ocean tile
+      int N = 128;
+      double L = 256.0;
+      double u = 5.0;
+
+      this->oceanTile.reset(new rendering::OceanTile(N, L));
+      this->oceanTile->SetWindVelocity(u, 0.0);
+
+      // do not pass to common::MeshManager as we are going to modify
+      this->oceanTileMesh.reset(this->oceanTile->CreateMesh());
+
+      // create visual
+      rendering::Ogre2OceanVisualPtr ogre2Visual =
+          std::make_shared<rendering::Ogre2OceanVisual>(); 
+
+      // Scene: initialisation work-around
+      rendering::Ogre2ScenePtr ogre2Scene =
+          std::dynamic_pointer_cast<rendering::Ogre2Scene>(this->scene);
+      ogre2Visual->InitObject(ogre2Scene, 50010, "ocean-visual");
+      // visual->LoadCube();
+      // ogre2Visual->LoadOceanTile(this->oceanTile);
+      ogre2Visual->LoadMesh(this->oceanTileMesh);
+
+      rendering::VisualPtr visual = ogre2Visual;
+      visual->SetLocalPosition(0.0, 0.0, 0.0);
+      visual->SetLocalRotation(0.0, 0.0, 0.0);
+      visual->SetLocalScale(1.0, 1.0, 1.0);
+      visual->SetMaterial("OceanBlue");
+      
+
+      // Required? These are used in ignition::gazebo::VisualizationCapabilities
+      // ignition::gazebo::Entity entityId;
+      // visual->SetUserData("gazebo-entity", static_cast<int>(entityId));
+      // visual->SetUserData("pause-update", static_cast<int>(0));
+
+      // add visual to parent
+      // auto parent = this->visual->Parent();
+      // parent->AddChild(visual);
+
+      // keep reference
+      this->oceanVisual = visual;
+    }
+
+    if (!this->oceanVisual)
+      return;
+
+    // Simple update (reload entire mesh...)
+    // update the tile (recalculates vertices)
+    this->oceanTile->UpdateMesh(simTime, this->oceanTileMesh.get());
+
+    // update the dynamic renderable (CPU => GPU)
+    rendering::Ogre2OceanVisualPtr ogre2Visual =
+        std::dynamic_pointer_cast<rendering::Ogre2OceanVisual>(
+            this->oceanVisual);
+    // ogre2Visual->UpdateOceanTile(this->oceanTile);
+    ogre2Visual->UpdateMesh(this->oceanTileMesh);
+
+    break;
+  }
+  case OceanVisualMethod::OGRE1_MESH:
+  {
+    // Test attaching an Ogre2 mesh to the entity (import from Ogre::v1::Mesh)
+    if (!this->ogre2OceanTile)
+    {
+      ignmsg << "Waves: creating Ogre::v1::Mesh ocean visual\n";
+
+      // \todo(srmainwaring) synchronise visual with physics...
+      int N = 128;
+      double L = 256.0;
+      double u = 5.0;
+
+      // create static ocean tile using common::mesh - this to populate
+      // an object in the MeshManager with the correct name
+      this->oceanTile.reset(new rendering::OceanTile(N, L));
+      this->oceanTile->SetWindVelocity(u, 0.0);
+      std::unique_ptr<common::Mesh> newMesh(this->oceanTile->CreateMesh());
+      auto mesh = newMesh.get();
+      common::MeshManager::Instance()->AddMesh(newMesh.release());
+
+      ignmsg << "Waves: mesh name " << mesh->Name() << "\n";
+      ignmsg << "Waves: mesh resource path " << mesh->Path() << "\n";
+
+      this->ogre2OceanTile.reset(new rendering::Ogre2OceanTile(N, L));
+      this->ogre2OceanTile->SetWindVelocity(u, 0.0);
+      this->ogre2OceanTile->Create();
+      // this->ogre2OceanTile->Update(0.0);
+
+      // ogre2 specific
+      rendering::Ogre2ScenePtr ogre2Scene =
+          std::dynamic_pointer_cast<rendering::Ogre2Scene>(this->scene);    
+
+      auto meshFactory = rendering::Ogre2MeshFactoryExtPtr(
+            new rendering::Ogre2MeshFactoryExt(ogre2Scene));
+
+      rendering::MeshDescriptor meshDescriptor;
+      meshDescriptor.mesh = mesh;
+      meshDescriptor.meshName = "AboveOceanTileMesh"; // mesh->Name();
+
+      /////////////////////////////////////////////
+      // BEGIN_LAMDAS
+      //
+      // Override functions from Scene, BaseScene, 
+      // and Ogre2Scene
+      //
+      /////////////////////////////////////////////
+
+      // bool Ogre2Scene::InitObject(Ogre2ObjectPtr _object, unsigned int _id,
+      //     const std::string &_name)
+      auto Ogre2Scene_InitObject = [&](
+          rendering::Ogre2ScenePtr _scene,
+          rendering::Ogre2ObjectPtr _object,
+          unsigned int _id,
+          const std::string &_name) -> bool
       {
         ignmsg << "WavesVisual: creating dynamic geometry ocean visual\n";
 
@@ -577,34 +790,31 @@ void WavesVisualPrivate::OnUpdate()
         //convert common::Mesh to rendering::Mesh
         auto geometry = this->scene->CreateMesh(mesh);
 
-        // create ocean tile visual
-        auto visual = this->scene->CreateVisual("ocean-tile");
-        visual->AddGeometry(geometry);
-        visual->SetLocalPosition(0.0, 0.0, 0.0);
-        visual->SetLocalRotation(0.0, 0.0, 0.0);
-        visual->SetLocalScale(1.0, 1.0, 1.0);
-        visual->SetMaterial("OceanBlue");
-        visual->SetVisible(true);
+      //convert common::Mesh to rendering::Mesh
+      auto geometry = this->scene->CreateMesh(mesh);
+      geometry->SetMaterial("OceanBlue");
 
-        // // retrive the material from the visual's geometry (it's not set on the visual)
-        // ignmsg << "WavesVisual: Visual Name:          " << this->visual->Name() << "\n";
-        // ignmsg << "WavesVisual: Visual GeometryCount: " << this->visual->GeometryCount() << "\n";
-        // auto visualGeometry = this->visual->GeometryByIndex(0);
-
-        // auto material = visualGeometry->Material();
-        // // auto material = this->visual->Material();
-        // if (!material)
-        //   ignerr << "WavesVisual: invalid material\n";
-        // else
-        //   visual->SetMaterial(material);
-
-        // add visual to parent
-        auto parent = this->visual->Parent();
-        parent->AddChild(visual);
-
-        // keep reference
-        this->oceanVisual = visual;
+      // remove existing geometry
+      auto visual = this->visual;
+      if (visual->GeometryCount() > 0)
+      {
+        for (auto i=0; i<visual->GeometryCount(); ++i)
+        {
+          visual->RemoveGeometryByIndex(i);
+        }
       }
+
+      // create ocean tile visual
+      // auto visual = this->scene->CreateVisual("ocean-tile");
+
+      visual->AddGeometry(geometry);
+      visual->SetLocalPosition(0.0, 0.0, 0.0);
+      visual->SetLocalRotation(0.0, 0.0, 0.0);
+      visual->SetLocalScale(1.0, 1.0, 1.0);
+
+      // add visual to parent
+      // auto parent = this->visual->Parent();
+      // parent->AddChild(visual);
 
       if (!this->oceanTile)
         return;
@@ -613,27 +823,249 @@ void WavesVisualPrivate::OnUpdate()
       this->oceanTile->Update(simTime);
       break;
     }
-    default:
+
+    if (!this->oceanTile)
+      return;
+
+    // Update the tile
+    this->oceanTile->Update(simTime);
+    break;
+  }
+  case OceanVisualMethod::PLANE:
+  {
+    // Test attaching another visual to the entity
+    if (!this->oceanVisual)
     {
       // Test attaching another visual to the entity
       if (!this->oceanVisual)
       {
         ignmsg << "WavesVisual: creating default ocean visual\n";
 
-        // create plane
-        auto geometry = this->scene->CreatePlane();
+      // create plane
+      auto geometry = this->scene->CreatePlane();
+      geometry->SetMaterial("OceanBlue");
 
-        // create visual
-        auto visual = this->scene->CreateVisual("ocean-tile");
-        visual->AddGeometry(geometry);
-        visual->SetLocalPosition(0.0, 0.0, 0.0);
-        visual->SetLocalRotation(0.0, 0.0, 0.0);
-        visual->SetLocalScale(100.0, 100.0, 100.0);
-        visual->SetMaterial("OceanBlue");
+      // remove existing geometry
+      auto visual = this->visual;
+      if (visual->GeometryCount() > 0)
+      {
+        for (auto i=0; i<visual->GeometryCount(); ++i)
+        {
+          visual->RemoveGeometryByIndex(i);
+        }
       }
-      break;
+
+      // create visual
+      // auto visual = this->scene->CreateVisual("ocean-tile");
+
+      visual->AddGeometry(geometry);
+      visual->SetLocalPosition(0.0, 0.0, 0.0);
+      visual->SetLocalRotation(0.0, 0.0, 0.0);
+      visual->SetLocalScale(100.0, 100.0, 100.0);
+
+      // See SceneManager.cc L196-198
+      // visual->SetUserData("gazebo-entity", static_cast<int>(this->oceanEntity));
+      // visual->SetUserData("pause-update", static_cast<int>(0));
+
+      // visual->SetMaterial("OceanBlue");
+      // visual->SetMaterial(this->material);
+
+      // auto material = this->visual->Material();
+      // if (material)
+      // {
+      //   ignmsg << "Waves: set material [" << material->Name() << "]\n";
+      //   visual->SetMaterial(material);
+      // }
+
+      // keep reference
+      this->oceanVisual = visual;
+    }
+
+    if (!this->oceanVisual)
+      return;
+
+    break;
+  }
+  default:
+    break;
+  }
+
+
+  ////////// BEGIN SHADER_PARAM
+
+  // get the material and set shaders
+  if (!this->material)
+  {
+    auto mat = scene->CreateMaterial();
+    mat->SetVertexShader(this->vertexShaderUri);
+    mat->SetFragmentShader(this->fragmentShaderUri);
+    this->visual->SetMaterial(mat);
+    scene->DestroyMaterial(mat);
+    this->material = this->visual->Material();
+    ignmsg << "Waves: create material [" << this->material->Name() << "]\n";
+  }
+
+  if (!this->material)
+    return;
+
+  // set the shader params read from SDF
+  // this is only done once
+  for (const auto & spv : this->shaderParams)
+  {
+    std::vector<std::string> values = common::split(spv.value, " ");
+
+    int intValue = 0;
+    float floatValue = 0;
+    std::vector<float> floatArrayValue;
+    std::string stringValue = "";
+
+    rendering::ShaderParam::ParamType paramType =
+        rendering::ShaderParam::PARAM_NONE;
+
+    rendering::ShaderParamsPtr params;
+    if (spv.shader == "fragment")
+    {
+      params = this->material->FragmentShaderParams();
+    }
+    else if (spv.shader == "vertex")
+    {
+      params = this->material->VertexShaderParams();
+    }
+
+    // if no <value> is specified, this could be a constant
+    if (values.empty())
+    {
+      // \todo handle args for constants
+      (*params)[spv.name] = intValue;
+    }
+    // float / int
+    else if (values.size() == 1u)
+    {
+      std::string str = values[0];
+
+      // TIME is reserved keyword for sim time
+      if (str == "TIME")
+      {
+        this->timeParams.push_back(spv);
+        continue;
+      }
+
+      // if <type> is not empty, respect the specified type
+      if (!spv.type.empty())
+      {
+        if (spv.type == "int")
+        {
+          intValue = std::stoi(str);
+          paramType = rendering::ShaderParam::PARAM_INT;
+        }
+        else if (spv.type == "float")
+        {
+          floatValue = std::stof(str);
+          paramType = rendering::ShaderParam::PARAM_FLOAT;
+        }
+        else if (spv.type == "texture")
+        {
+          stringValue = str;
+          paramType = rendering::ShaderParam::PARAM_TEXTURE;
+        }
+        else if (spv.type == "texture_cube")
+        {
+          stringValue = str;
+          paramType = rendering::ShaderParam::PARAM_TEXTURE_CUBE;
+        }
+        else
+        {
+          // \todo(anyone) support texture samplers
+        }
+      }
+      // else do our best guess at what the type is
+      else
+      {
+        std::string::size_type sz;
+        int n = std::stoi(str, &sz);
+        if ( sz == str.size())
+        {
+          intValue = n;
+          paramType = rendering::ShaderParam::PARAM_INT;
+        }
+        else
+        {
+          floatValue = std::stof(str);
+          paramType = rendering::ShaderParam::PARAM_FLOAT;
+        }
+      }
+    }
+    // arrays
+    else
+    {
+      // int array
+      if (!spv.type.empty() && spv.type == "int_array")
+      {
+        for (const auto &v : values)
+          floatArrayValue.push_back(std::stoi(v));
+        paramType = rendering::ShaderParam::PARAM_INT_BUFFER;
+      }
+      // treat everything else as float_array
+      else
+      {
+        for (const auto &v : values)
+          floatArrayValue.push_back(std::stof(v));
+        paramType = rendering::ShaderParam::PARAM_FLOAT_BUFFER;
+      }
+    }
+
+    // set the params
+    if (paramType == rendering::ShaderParam::PARAM_INT)
+    {
+      (*params)[spv.name] = intValue;
+    }
+    else if (paramType == rendering::ShaderParam::PARAM_FLOAT)
+    {
+      (*params)[spv.name] = floatValue;
+    }
+    else if (paramType == rendering::ShaderParam::PARAM_TEXTURE)
+    {
+      std::string textureUri = common::findFile(
+          asFullPath(stringValue, this->modelPath));
+
+      // \todo(anyone) support selecting texture coordinate set
+      (*params)[spv.name].SetTexture(textureUri,
+          rendering::ShaderParam::ParamType::PARAM_TEXTURE, 0u);
+    }
+    else if (paramType == rendering::ShaderParam::PARAM_TEXTURE_CUBE)
+    {
+      std::string textureUri = common::findFile(
+          asFullPath(stringValue, this->modelPath));
+
+      // \todo(anyone) support selecting texture coordinate set
+      (*params)[spv.name].SetTexture(textureUri,
+          rendering::ShaderParam::ParamType::PARAM_TEXTURE_CUBE, 1u);
+    }
+    else if (paramType == rendering::ShaderParam::PARAM_INT_BUFFER ||
+        paramType == rendering::ShaderParam::PARAM_FLOAT_BUFFER)
+    {
+      (*params)[spv.name].InitializeBuffer(floatArrayValue.size());
+      float *fv = &floatArrayValue[0];
+      (*params)[spv.name].UpdateBuffer(fv);
     }
   }
+  this->shaderParams.clear();
+
+  // time variables need to be updated every iteration
+  for (const auto & spv : this->timeParams)
+  {
+    float floatValue = (std::chrono::duration_cast<std::chrono::nanoseconds>(
+        this->currentSimTime).count()) * 1e-9;
+    rendering::ShaderParamsPtr params;
+    if (spv.shader == "fragment")
+      params = this->material->FragmentShaderParams();
+    else if (spv.shader == "vertex")
+      params = this->material->VertexShaderParams();
+    (*params)[spv.name] = floatValue;
+  }
+
+  ////////// END SHADER_PARAM
+
 }
 
 //////////////////////////////////////////////////
